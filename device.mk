8dc62645be171933129b45003e3980c8299df07f
PIPIPIG233666 <pig.priv@gmail.com>
2023-02-20 15:43:50 +0800

    lavender: device: Place a copy of audio platform info intcodec as audio platform info
    Silence following:
    * D platform_info: platform_info_init: Failed to open
    audio_platform_info.xml, using defaults.
    
    Change-Id: Icf9bb063d1bef72143c01009da17727f5b6fdefd

--- device.mk
+++ device.mk
@@ -29,6 +29,7 @@ ENABLE_APEX := true
 # Audio
 PRODUCT_COPY_FILES += \
 	$(DEVICE_PATH)/audio/audio_platform_info_intcodec.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_platform_info_intcodec.xml \
+	$(DEVICE_PATH)/audio/audio_platform_info_intcodec.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_platform_info.xml \
 	$(DEVICE_PATH)/audio/mixer_paths.xml:$(TARGET_COPY_OUT_VENDOR)/etc/mixer_paths.xml
 
 # Boot animation
